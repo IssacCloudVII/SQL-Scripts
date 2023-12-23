cc19b948511c3414f86775b03e2daf34e00a8f0b
Héctor Quezada Alanís <hectoriinq@hotmail.com>
2023-12-22 20:45:32 -0600

    Selects and basic aggregates

---
+++ Select.sql
@@ -0,0 +1,26 @@
+--Select * from EmployeeDemographics
+
+--Select top 5 * from EmployeeSalary
+
+--DECLARE @RowCount INT = 5;
+
+--SELECT TOP (@RowCount) EmployeeID, FirstName, LastName
+--FROM EmployeeDemographics
+--ORDER BY EmployeeID;
+
+--SELECT TOP 15 PERCENT EmployeeID, FirstName, LastName
+--FROM EmployeeDemographics
+--ORDER BY Age DESC;
+
+--Select distinct Gender from EmployeeDemographics
+
+--Select distinct FirstName, Gender from EmployeeDemographics
+
+--Select count (LastName) as LastNameCount from EmployeeDemographics
+
+--Select count (*) as MaleCount from EmployeeDemographics where Gender = 'Male'
+
+--Select Max(Salary) from EmployeeSalary
+--Select Min(Salary) from EmployeeSalary
+--Select Avg(Salary) from EmployeeSalary
+
