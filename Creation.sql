2dd92c5010a089c0877dea23f5ad1a1193152e9b
Héctor Quezada Alanís <hectoriinq@hotmail.com>
2023-12-22 20:32:18 -0600

    Create and Insert scripts

---
+++ Creation.sql
@@ -0,0 +1,41 @@
+--Create table EmployeeDemographics
+--(
+--EmployeeID int, 
+--FirstName varchar(50),
+--Lastname varchar(50),
+--Age int, 
+--Gender varchar(50)
+--)
+
+--Select * from INFORMATION_SCHEMA.tables
+
+--Create table EmployeeSalary
+--(EmployeeID int,
+--JobTitle varchar(50),
+--Salary int)
+
+---- Inserts for EmployeeDemographics
+--INSERT INTO EmployeeDemographics (EmployeeID, FirstName, Lastname, Age, Gender)
+--VALUES (1, 'John', 'Doe', 28, 'Male'),
+--       (2, 'Jane', 'Smith', 35, 'Female'),
+--       (3, 'Bob', 'Johnson', 42, 'Male'),
+--       (4, 'Alice', 'Williams', 29, 'Female'),
+--       (5, 'Charlie', 'Brown', 34, 'Male'),
+--       (6, 'Eva', 'Davis', 31, 'Female'),
+--       (7, 'David', 'Miller', 45, 'Male'),
+--       (8, 'Grace', 'Jones', 39, 'Female'),
+--       (9, 'Frank', 'Taylor', 26, 'Male'),
+--       (10, 'Olivia', 'White', 37, 'Female');
+
+---- Inserts for EmployeeSalary
+--INSERT INTO EmployeeSalary (EmployeeID, JobTitle, Salary)
+--VALUES (1, 'Software Engineer', 80000),
+--       (2, 'Data Analyst', 65000),
+--       (3, 'Project Manager', 90000),
+--       (4, 'Marketing Specialist', 75000),
+--       (5, 'System Administrator', 70000),
+--       (6, 'Financial Analyst', 85000),
+--       (7, 'HR Manager', 95000),
+--       (8, 'Product Manager', 100000),
+--       (9, 'Network Engineer', 72000),
+--       (10, 'Sales Representative', 80000);
